f965a7e802fd6ebc7ce9a87bbdee6932a7dc87b2
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-10-30 21:05:11 +0100

    Refactored Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+const name = "bundle";
+
+module.exports = (env, { mode }) => {
+	const is_production = mode === "production";
+
+	const config = {
+		watch: !is_production,
+
+		entry: path.resolve(__dirname, "src/entry.ts"),
+
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: `${name}.js`,
+		},
+
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "src"),
+			},
+		},
+
+		module: { rules: [] },
+
+		plugins: [],
+	};
+
+	config.module.rules.push({
+		test: /\.tsx?$/,
+		exclude: /node_modules/,
+		loader: "babel-loader",
+		resolve: {
+			extensions: [".ts", ".tsx", ".js", ".jsx"],
+		},
+	});
+
+	config.module.rules.push({
+		test: /\.(css|styl)$/,
+		use: [
+			MiniCssExtractPlugin.loader,
+
+			{
+				loader: "css-loader",
+				options: {
+					modules: {
+						localIdentName: is_production
+							? "[hash:base64:5]"
+							: "[name]-[local]-[hash:base64:2]",
+					},
+				},
+			},
+
+			{
+				loader: "stylus-loader",
+				options: {
+					stylusOptions: {
+						use: "nib",
+					},
+				},
+			},
+		],
+	});
+
+	config.plugins.push(
+		new MiniCssExtractPlugin({
+			filename: `${name}.css`,
+		})
+	);
+
+	if (is_production) {
+		config.optimization = {
+			minimize: true,
+			minimizer: [
+				new OptimizeCSSAssetsPlugin(),
+
+				// As we are using a custom optimization, making use of
+				// OptimizeCSSAssetsPlugin, we also need to specify TerserPlugin
+				new TerserPlugin({ extractComments: false }),
+			],
+		};
+	}
+
+	return config;
+};
