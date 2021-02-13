12ad687d77c45d813fa78ac66e3ce03834a298f2
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-02-13 15:46:42 +0100

    Removed assets from static directories

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 7000;
 const DIRECTORY_DIST = path.join(__dirname, "dist");
-const DIRECTORY_ASSETS = path.join(__dirname, "assets");
 
 /**
  * Static directories
  */
 app.use(express.static(DIRECTORY_DIST));
-app.use(express.static(DIRECTORY_ASSETS));
 
 /**
  * Routes
